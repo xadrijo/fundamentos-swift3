56594a2a801d77ec53189f700ef037e9927e5d08
Jorge Jimenez <xadrijo@gmail.com>
2017-01-06 00:19:43 -0400

    first commit

---
+++ 02-variables-constantes.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var entero = 23
+var miDouble = 2.00
+var miFloat = 3.142567
+var miBool = true
+var miString = "Cadena de Caracteres"
+
+let letEntero = 3
+let letDouble = 3.00
+let letFloat = 45.253656
+let letBool = false
+let letString = "Cadena de caracteres constante"
+
+var nombre: String = "George"
+let capacidad: Int = 400
