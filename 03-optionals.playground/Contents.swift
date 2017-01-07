2059f763546252195a3bd97ab5dd1ec2bbafa221
Jorge Jimenez <xadrijo@gmail.com>
2017-01-07 01:12:16 -0400

    Optionals tutorial

---
+++ 03-optionals.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//: Optionals: Los optionals se usan en situaciones donde puede haber ausencia de valor.
+let miEdad = "34"
+
+let edadNumerica = Int(miEdad)
+
+print(edadNumerica!)
+
+var apodo: String?
+
+print(apodo)
+
